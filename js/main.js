a6c5061125c957c8dbe5774d8793de919e76b0d1
Murod1303 <murodshernazaroff@gmail.com>
2023-07-05 19:26:02 +0500

    Add footer and carusel

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.about__list').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+  });
